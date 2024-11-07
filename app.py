534132cc5a65cf8a116eecfa8520d7fb7e97d19f
JavadRahimi14 <javadgunner14@gmail.com>
2024-11-07 22:17:01 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/data')
+@app.route('/')
 def index():
     return render_template('index.html')  # This will render templates/index.html
 
